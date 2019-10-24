03c5c71edc892fd0cd51f7621fab4844decd9c6c
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2019-10-24 13:48:30 +0100

    Update film link

--- app/connectors/ImdbConnector.scala
+++ app/connectors/ImdbConnector.scala
@@ -11,18 +11,14 @@ class ImdbConnector @Inject()(wsClient: WSClient) {
 
   def getFilms(): Future[List[Film]] = {
 
-//    val regex = """<h4><span class="unbold">(\d+\.)<\/span>([\w '!?,-:]+)<span class="unbold">(\([\d]{4}\))<\/span><\/h4>""".stripMargin.r("number", "title", "year")
-    val regex = """<a href="(\S+)"class="btn-full" > <span class="media-body media-vertical-align"><h4><span class="unbold">(\d+\.)<\/span>([\w '!?,-:]+)<span class="unbold">(\([\d]{4}\))<\/span><\/h4>""".stripMargin.r("url", "number", "title", "year")
+    val regex = """<a href="\/title\/(\S+)\/\S+"class="btn-full" > <span class="media-body media-vertical-align"><h4><span class="unbold">(\d+\.)<\/span>([\w '!?,-:]+)<span class="unbold">(\([\d]{4}\))<\/span><\/h4>""".stripMargin.r("titleId", "number", "title", "year")
 
     wsClient.url("https://m.imdb.com/chart/top").get.map { response =>
       val body = response.body.replaceAll("\n", "")
 
-      regex.findAllMatchIn(body).map(m => (m.group("title"), s"https://m.imdb.com${m.group("url")}")).toList
+      regex.findAllMatchIn(body).map(m => (m.group("title"), s"https://www.imdb.com/title/${m.group("titleId")}")).toList
         .filterNot(_._1.matches(".*[0-9].*"))
         .map(filmData => Film(filmData._1, filmData._2))
-
-//      regex.findAllMatchIn(body).map(_.group("title")).toList.filterNot(_.matches(".*[0-9].*"))
-
     }
   }
 }
