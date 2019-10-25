427f4d363ef5db39126d07b7b2032bdfdb0704e7
Liam Oldfield <liam.oldfield@digital.hmrc.gov.uk>
2019-10-25 13:30:20 +0100

    Auth action as filter

--- app/controllers/auth/AuthAction.scala
+++ app/controllers/auth/AuthAction.scala
@@ -9,17 +9,13 @@ import scala.concurrent.{ExecutionContext, Future}
 
 class AuthAction @Inject()(val parser: BodyParsers.Default,
                            appConfig: ApplicationConfig)(implicit val executionContext: ExecutionContext)
-  extends ActionBuilder[Request, AnyContent] with ActionRefiner[Request, Request] {
+  extends ActionBuilder[Request, AnyContent] with ActionFilter[Request] {
 
-  override protected def refine[A](request: Request[A]): Future[Either[Result, Request[A]]] = {
-
-    val sessionUUID = request.session.get("UUID")
-
-    sessionUUID.fold[Future[Either[Result, Request[A]]]] {
-      Future.successful(Left(Redirect(controllers.routes.HomeController.createSession())))
-    } {
-      _ =>
-        Future.successful(Right(request))
-    }
+  override def filter[A](request: Request[A]): Future[Option[Result]] = {
+    Future.successful(
+      request.session.get("UUID")
+      .fold[Option[Result]](
+        Some(Redirect(controllers.routes.HomeController.createSession())))(
+        _ => None))
   }
 }
